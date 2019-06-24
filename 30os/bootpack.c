0f9c74c40192456edd5c60b65ed7717d635ad0cc
Takahiro Sugaya <takskg@gmail.com>
2019-06-25 04:02:37 +0900

    add:マウスの割り込み追加

--- 30os/bootpack.c
+++ 30os/bootpack.c
@@ -1,13 +1,56 @@
 #include <stdio.h>
 #include "bootpack.h"
 
-extern struct KEYBUF keybuf;
+extern struct FIFO8 keyfifo;
+extern struct FIFO8 mousefifo;
+
+
+#define PORT_KEYDAT				0x0060
+#define PORT_KEYSTA				0x0064
+#define PORT_KEYCMD				0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE		0x60
+#define KBC_MODE				0x47
+
+void wait_KBC_sendready(void)
+{
+	/* キーボードコントローラがデータ送信可能になるのを待つ */
+	for (;;) {
+		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+			break;
+		}
+	}
+	return;
+}
+
+void init_keyboard(void)
+{
+	/* キーボードコントローラの初期化 */
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, KBC_MODE);
+	return;
+}
+
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+void enable_mouse(void)
+{
+	/* マウス有効 */
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	return; /* うまくいくとACK(0xfa)が送信されてくる */
+}
 
 
 void HariMain(void)
 {
 	struct BOOTINFO* binfo = (struct BOOTINFO *)ADR_BOOTINFO;
-	char s[40], mcursor[256];
+	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my;
 	unsigned char keycode;
 	int keyCount;
@@ -15,7 +58,15 @@ void HariMain(void)
 	init_gdtidt();
 	init_pic();
 	io_sti(); /* IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除 */
+
+	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 128, mousebuf);
 	
+	io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+	
+	init_keyboard();
+
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
 	mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
@@ -25,23 +76,26 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
-	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+	enable_mouse();
 
 	for(;;){
 		io_cli();
-		if (keybuf.next == 0) {
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
 			io_stihlt();
 		} else {
-			keycode = keybuf.data[0];
-			keybuf.next--;
-			for (keyCount = 0; keyCount < keybuf.next; keyCount++) {
-				keybuf.data[keyCount] = keybuf.data[keyCount + 1];
+			if (fifo8_status(&keyfifo) != 0) {
+				keycode = fifo8_get(&keyfifo);
+				io_sti();
+				sprintf(s, "%02X", keycode);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484,  0, 16, 15, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+			} else if (fifo8_status(&mousefifo) != 0) {
+				keycode = fifo8_get(&mousefifo);
+				io_sti();
+				sprintf(s, "%02X", keycode);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
 			}
-			io_sti();
-			sprintf(s, "%02X", keycode);
-			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
 		}
 	}
 }
