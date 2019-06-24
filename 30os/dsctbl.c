5eb72566e7449260c2d30c3fd47dad0c600d2959
Takahiro Sugaya <takskg@gmail.com>
2019-06-25 02:12:02 +0900

    update:コード整理

--- 30os/dsctbl.c
+++ 30os/dsctbl.c
@@ -1,26 +1,4 @@
-struct SEGMENT_DESCRIPTOR {
-	short	limit_low;
-	short	base_low;
-	char	base_mid;
-	char	access_right;
-	char	limit_high;
-	char	base_high;
-};
-
-struct GATE_DESCRIPTOR {
-	short	offset_low;
-	short	selector;
-	char	dw_count;
-	char	access_right;
-	short	offset_high;
-};
-
-
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
-
+#include "bootpack.h"
 
 
 void init_gdtidt(void)
