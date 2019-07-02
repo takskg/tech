488b69f7e5b3fe559ddc5b23ad3726403a0e4435
Takahiro Sugaya <takskg@gmail.com>
2019-07-03 03:18:25 +0900

    add:ウィンドウがを出してみた

--- 30os/bootpack.h
+++ 30os/bootpack.h
@@ -168,6 +168,7 @@ struct SHEET {
 	int col_inv;
 	int height;
 	int flags;
+	struct SHTCTL* ctl;
 };
 struct SHTCTL {
 	unsigned char *vram;
@@ -180,7 +181,7 @@ struct SHTCTL {
 struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
 struct SHEET *sheet_alloc(struct SHTCTL *ctl);
 void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
-void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
-void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1);
-void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
-void sheet_free(struct SHTCTL *ctl, struct SHEET *sht);
+void sheet_updown(struct SHEET *sht, int height);
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void sheet_slide(struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHEET *sht);
